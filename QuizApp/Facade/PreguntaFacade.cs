f00f085283cd8c9510ef5c8867bf4294998b472f
Victoria Derudder <32909661+vderudder@users.noreply.github.com>
2023-03-09 16:10:43 -0300

    wip: utilizando dummy data como dto

--- QuizApp/Facade/PreguntaFacade.cs
+++ QuizApp/Facade/PreguntaFacade.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QuizApp.Dominio;
+using QuizApp.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +10,39 @@ namespace QuizApp.Facade
 {
     internal class PreguntaFacade
     {
-        public string[] getPreguntas()
+        public List<Pregunta> getPreguntas()
         {
-            return new string[] { "test1" };
+            var storage = new PreguntaStorage();
+            var preguntaDTOs = storage.getPreguntasByFiltro(new PreguntaFiltro("1", "1", 3));
+
+            var preguntas = preguntaDTOs.Select(dto => new Pregunta(
+                                                dto.iPregunta,
+                                                getCategoriaById(dto.iCategoriaId),
+                                                getDificultadById(dto.iDificultadId),
+                                                getTipoPreguntaById(dto.iTipoPreguntaId),
+                                                dto.iCorrecta,
+                                                dto.iIncorrectaList.ToList())
+                                                ).ToList();
+
+            return preguntas;
         }
 
+        private Categoria getCategoriaById(string pId)
+        {
+            return new Categoria(pId, "Matematicas");
+        }
+
+        private Dificultad getDificultadById(string pId)
+        {
+            return new Dificultad(pId, "Facil");
+
+        }
+        private TipoPregunta getTipoPreguntaById(string pId)
+        {
+            return new TipoPregunta(pId, "Multiple");
+
+        }
+
+       
     }
 }
---
+++ QuizApp/Storage/PreguntaStorage.cs
@@ -0,0 +1,69 @@
+﻿using QuizApp.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Storage
+{
+    internal class PreguntaStorage
+    {
+        /// <summary>
+        /// Obtiene el DTO de las preguntas con el filtro correspondiente
+        /// </summary>
+        /// <param name="pFiltro"></param>
+        /// <returns></returns>
+        public PreguntaDTO[] getPreguntasByFiltro(PreguntaFiltro pFiltro)
+        {
+            var pregunta1 = new PreguntaDTO(
+                "Cuanto es 2+2?",
+                "1",
+                "1",
+                "1",
+                // respuesta correcta
+                "4",
+                //respuestas incorrectas
+                new List<string> {"5", "2", "20"}
+                );
+
+            return new PreguntaDTO[] { pregunta1 };
+        }
+    }
+
+    internal class PreguntaFiltro
+    {
+        // Atributos
+        public string iDificultad;
+        public string iCategoria;
+        public int iCantidad;
+
+        public PreguntaFiltro(string pDificultad, string pCategoria, int pCantidad)
+        {
+            this.iDificultad = pDificultad;
+            this.iCategoria = pCategoria;
+            this.iCantidad = pCantidad;
+        }
+    }
+
+    internal class PreguntaDTO
+    {
+        // Atributos
+        public string iPregunta;
+        public string iDificultadId;
+        public string iCategoriaId;
+        public string iTipoPreguntaId;
+        public string iCorrecta;
+        public List<string> iIncorrectaList;
+
+        public PreguntaDTO(string pPregunta, string pDificultadId, string pCategoriaId, string pTipoPreguntaId, string pCorrecta, List<string> pIncorrectaList)
+        {
+            this.iPregunta = pPregunta;
+            this.iDificultadId = pDificultadId;
+            this.iCategoriaId = pCategoriaId;
+            this.iTipoPreguntaId = pTipoPreguntaId;
+            this.iCorrecta = pCorrecta;
+            this.iIncorrectaList = pIncorrectaList;
+        }
+    }
+}
